22157afe9e91c69657eac56d4e7746f5913d714e
Poporacer <46263877+Poporacer@users.noreply.github.com>
2021-08-07 15:52:51 -0600

    Update fpp_install.sh

--- fpp_install.sh
+++ fpp_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 pushd $(dirname $(which $0))
 target_PWD=$(readlink -f .)
+. /opt/fpp/scripts/common
+setSetting restartFlag 1
 exec /opt/fpp/scripts/update_plugin ${target_PWD##*/}
 
 popd
